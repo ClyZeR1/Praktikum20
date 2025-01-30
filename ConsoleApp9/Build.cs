d44bc86e641833de72a00f6f01fd57d5db19d5a5
exuteboll <exuteboll4@gmail.com>
2025-01-30 19:17:18 +0200

    Add files via upload

---
+++ ConsoleApp9/Build.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Build
+    {
+        public string name;
+
+        public double area;
+
+        public int kvo;
+
+        public int floor;
+
+        public Build() { }
+
+        public Build(string name)
+        {
+            this.name = name;
+        }
+        public Build(string name, double area) : this(name)
+        {
+            this.area = area;
+        }
+        public Build(string name, double area, int kvo) : this(name, area) 
+        {
+            this.kvo = kvo;
+        }
+        public Build(string name, double area, int kvo, int floor) : this(name, area, kvo)
+        {
+            this.floor = floor;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"В доме: {name}\t Площадью: {area}\t Квадратных метров живет: {kvo} чел," +
+                $"на человека - {area/ kvo:f2} кв. метров");
+               
+        }
+        public override string ToString()
+        {
+            return $"В доме {name} площадью {area} кв. метров живёт {kvo} чел, " +
+                $"на человека - {area / kvo:f2} кв. метров";
+        }
+
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Build dom1 = new Build();
+            dom1.name = "Квартира";
+            dom1.area = 80;
+            dom1.kvo = 2;
+            dom1.ShowInfo();
+            Console.WriteLine();
+            // второй дом
+            Build dom2 = new Build("Отель", 6000, 1500);
+            Console.WriteLine($"{dom2.ToString()}");
+            Console.WriteLine();
+            Console.Write("Название строения: ");
+            string name = Console.ReadLine();
+            Console.Write("Площадь строения: ");
+            double area = double.Parse(Console.ReadLine());
+            Console.Write("Количество жильцов: ");
+            int kvo = int.Parse(Console.ReadLine());
+            Console.Write("Количество этажей: ");
+            int floor = int.Parse(Console.ReadLine());
+            Build dom3 = new Build(name, area, kvo, floor);
+            Console.WriteLine($"{dom3.ToString()}");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
