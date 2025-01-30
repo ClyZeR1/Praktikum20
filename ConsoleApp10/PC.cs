52aaa51dc5e5bb0da7a182a23a4772b2eca3d6c0
exuteboll <exuteboll4@gmail.com>
2025-01-30 21:01:29 +0200

    Add files via upload

---
+++ ConsoleApp10/PC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp10
+{
+    internal class PC
+    {
+        public string model;
+        public int MGz;
+        public int OZY;
+        public int memory;
+        public PC() { }
+        public PC(string model)
+        {
+            this.model = model;
+        }
+        public PC(string model, int MGz) :this(model)
+            {
+            this.MGz = MGz;
+            }
+        public PC(string model, int MGz, int OZY) :this(model,MGz)
+        {
+            this.OZY = OZY;
+        }
+        public PC(string model, int MGz, int OZY, int memory) :this(model,MGz,OZY)
+        {
+            this.memory = memory;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Модель: {model}\t тактовая частота: {MGz}\t объем оперативной памяти: {OZY}\t Объем жесткого диска: {memory}");
+        }
+        public override string ToString()
+        {
+            return $"Модель: {model}\t тактовая частота: {MGz}\t объем оперативной памяти: {OZY}\t Объем жесткого диска: {memory}";
+        }
+
+
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PC pc = new PC();
+            pc.model = "AMD";
+            pc.MGz = 2;
+            pc.OZY = 8;
+            pc.memory = 256;
+            
+            Console.WriteLine();
+           //
+            Console.WriteLine("Модель: ");
+            string modeli = Console.ReadLine();
+            Console.WriteLine("Тактовая частота: ");
+            int mgz = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оперативная память: ");
+            int ozy = int.Parse(Console.ReadLine());
+            Console.WriteLine("Объем памяти: ");
+            int memory = int.Parse(Console.ReadLine());
+            pc.Info();
+            PC pc2 = new PC(modeli, mgz,ozy,memory);
+            Console.WriteLine($"{pc2.ToString()}");
+            //
+            PC pc3 = new PC("Intel", 3,32,512);
+            Console.WriteLine($"{pc3.ToString()}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
